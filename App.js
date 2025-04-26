c456ec4415f1a911694ff4d4c3ee8f2a92369eac
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2025-04-26 18:40:15 +0200

    stuff

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  Image,
+  SafeAreaView 
+} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>ASD</Text>
+      <Image source={require('./assets/favicon.png')}/>
+      <Image borderRadius={15} blurRadius={2} source={{
+        width: 200,
+        height: 200,
+        uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
     </SafeAreaView>
   );
 }
